4a0320e42962a3a2533c4a4c5e92fc27d613a334
ljferreyrac <lferreyrac04@hotmail.com>
2024-05-01 18:47:33 -0500

    Update BackendTheraphyApplication

--- src/main/java/com/theraphy/backend/BackendTheraphyApplication.java
+++ src/main/java/com/theraphy/backend/BackendTheraphyApplication.java
@@ -2,7 +2,10 @@ package com.theraphy.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -11,6 +14,16 @@ public class BackendTheraphyApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendTheraphyApplication.class, args);
 
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+            }
+        };
     }
 
 }
